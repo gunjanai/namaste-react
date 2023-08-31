f817a32488f1b0213fb532dfd038190fb38a1ff8
Gunjan Sengar <gunjansengar@Gunjans-MacBook-Pro.local>
2023-08-31 16:50:50 +0530

    Namaste react episode 1 adding more than 1 elements from React.createElement

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 const heading = React.createElement("h1", {id: "heading"}, "Hello world from react!");
 
-        const root = ReactDOM.createRoot(document.getElementById("root"));
+const nestedHeading = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id: "child1"}, "This is child 1"),
+    React.createElement("div", {id: "child2"}, "This is child 2")
+])
 
-        root.render(heading)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(nestedHeading)
