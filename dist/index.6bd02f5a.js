716d03c143f7993cf499ec1c7699220399f7f343
Gunjan Sengar <gunjansengar@Gunjans-MacBook-Pro.local>
2023-09-04 16:18:39 +0530

    parcel build added

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,17 @@
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "Hello world from react!");
+const nestedHeading = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child1"
+    }, "This is child 1"),
+    React.createElement("div", {
+        id: "child2"
+    }, "This is child 2")
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(nestedHeading);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
