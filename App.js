ea6d72be289fb06b9defc7932eba50b48e15bd8b
Gunjan Sengar <gunjansengar@Gunjans-MacBook-Pro.local>
2023-09-05 15:43:19 +0530

    Episode 3 starting commit, scripts for starting and building the project added in package.json

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
 const heading = React.createElement("h1", {id: "heading"}, "Hello world from react!");
 
 const nestedHeading = React.createElement("div", {id: "parent"}, [
